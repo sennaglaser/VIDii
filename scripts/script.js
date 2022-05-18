eabbe48c382671781bb883232ddcb9d730b36d20
sennaglaser <senna.glaser@hva.nl>
2022-05-18 09:58:05 +0200

    linkje aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ deButton1928.addEventListener("click", micky1928InBeeld);
 function micky1928InBeeld() {
 	deMicky.classList.remove("micky1935", "micky1999", "micky2020")
     deMicky.classList.toggle("micky1928")
-    tekstLink.href="https://www.nu.nl";
+    tekstLink.href="https://vandaagindegeschiedenis.nl/15-mei/";
     tekstLink.textContent = "Find out more about me";
 }
 
@@ -19,7 +19,7 @@ deButton1935.addEventListener("click", micky1935InBeeld);
 function micky1935InBeeld() {
     deMicky.classList.remove("micky1928", "micky1999", "micky2020");
 	deMicky.classList.toggle("micky1935");
-    tekstLink.href="https://www.nu.nl";
+    tekstLink.href="https://www.youtube.com/watch?v=4FvMCg_SIqw";
     tekstLink.textContent = "See more about me!";
 }
 
